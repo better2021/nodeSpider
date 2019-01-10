3f2526dad4d9489fcc1ca6925dad8f014195236d
huyafeng <709463253@qq.com>
2019-01-10 09:59:48 +0700

    'as'

--- src/demo.js
+++ src/demo.js
@@ -13,7 +13,7 @@ fs.readFile('../src/imgs/0.jpg', function(err, buffer) {
 //   console.log('已生成hiDir文件夹');
 // });
 
-//生成文件
+//生成文件;
 // fs.writeFile('./hiDir/message.txt', txt, 'utf-8', function(err) {
 //   if (err) throw err;
 //   console.log('已生成新的txt文件');
@@ -56,3 +56,15 @@ fs.stat('./hiDir/message.txt', function(err, stat) {
     }
   }
 });
+
+//let helloTxt = '这是hellotxt文件中的内容'
+//生成新的txt文件
+// fs.writeFile('./hiDir/hello.txt', helloTxt,'utf-8',function(err){
+//   if(err) throw err
+//   console.log('生成hellotxt文件')
+// })
+
+//const rs = fs.createReadStream('./hiDir/hello.txt', 'utf-8');
+//const ws = fs.createWriteStream('./hiDir/message.txt', 'utf-8');
+//让我们用pipe()把一个文件流和另一个文件流串起来，这样源文件的所有数据就自动写入到目标文件里了，所以，这实际上是一个复制文件的程序：
+//rs.pipe(ws);
