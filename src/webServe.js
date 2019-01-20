e8ed09659f59d64391310a40128640b8add47d61
huyafeng <709463253@qq.com>
2019-01-20 15:44:56 +0700

    'sas'

--- src/webServe.js
+++ src/webServe.js
@@ -1,14 +1,25 @@
 //引入HTTP模块
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
 //设置主机名
 const hostName = '127.0.0.1';
 //设置端口
-const port = 8081;
+const port = 8085;
 //创建服务
 let server = http.createServer((req, res) => {
-  //res.setHeader(200, { 'Content-Type': 'text/html' });
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>hello nodejs server!</h1>');
+  // let pathname = url.parse(req.url).pathname; // 解析请求，包括文件名
+  res.writeHead(200, { 'Content-Type': "text/html;charset='utf-8'" });
+  let filePath = path.join(__dirname, '/page', '/index.html'); //  绝对路劲
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      let text = '<h2>页面丢失404</h2>';
+      res.end(text.toString());
+    } else {
+      res.end(data.toString());
+    }
+  });
 });
 
 server.listen(port, hostName, () => {
